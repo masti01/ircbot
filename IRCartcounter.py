bc0b1ad79a30f604ff13072967abc813500042c4
masti01 <mastigm@gmail.com>
2021-12-16 22:31:44 +0100

    new branch

--- IRCartcounter.py
+++ IRCartcounter.py
@@ -12,8 +12,10 @@ http://python-irclib.sourceforge.net/
 # Author: Balasyum
 # http://hu.wikipedia.org/wiki/User:Balasyum
 # License : LGPL
-from __future__ import absolute_import, unicode_literals
-__version__ = '$Id: 229b3e02cf110f5e9d7f8d16c60906ee9769b7af $'
+
+# Author: masti
+# http://pl.wikipedia.org/wiki/User:masti
+# License : LGPL
 
 import re
 import threading
@@ -28,6 +30,7 @@ from time import strftime
 import irclib
 from ircbot import SingleServerIRCBot
 
+
 class ArtNoDisp(SingleServerIRCBot):
     
     def __init__(self, site, channel, nickname, server, port=6667):
@@ -35,8 +38,8 @@ class ArtNoDisp(SingleServerIRCBot):
         self.channel = channel
         self.site = site
         self.lang = site.language()
-        self.apiURL = u'https://'+self.lang+u'.'+site.family.name+u'.org/w/api.php?action=query&meta=siteinfo&siprop=statistics&format=xml'
-        self.logname = u'ircbot/artnos'+self.lang+u'.log'
+        self.apiURL = 'https://'+self.lang+'.'+site.family.name+'.org/w/api.php?action=query&meta=siteinfo&siprop=statistics&format=xml'
+        self.logname = 'ircbot/artnos'+self.lang+'.log'
 
         ns = []
         for n in site.namespaces():
@@ -44,19 +47,19 @@ class ArtNoDisp(SingleServerIRCBot):
                 ns += n[0]
             else:
                 ns += [n]
-        #self.other_ns = re.compile(u'14\[\[07(' + u'|'.join(ns) + u')')
+        #self.other_ns = re.compile('14\[\[07(' + '|'.join(ns) + ')')
         #self.api_url = self.site.api_address()
         #self.api_url += 'action=query&meta=siteinfo&siprop=statistics&format=xml'
         #self.api_found = re.compile(r'articles="(.*?)"')
         self.re_edit = re.compile(
             r'^C14\[\[^C07(?P<page>.+?)^C14\]\]^C4 (?P<flags>.*?)^C10 ^C02(?P<url>.+?)^C ^C5\*^C ^C03(?P<user>.+?)^C ^C5\*^C \(?^B?(?P<bytes>[+-]?\d+?)^B?\) ^C10(?P<summary>.*)^C'.replace(
-                '^B', '\002').replace('^C', '\003').replace('^U', '\037'))
+                '^B', '\002').replace('^C', '\003').replace('^', '\037'))
         self.re_move = re.compile(
-            ur'^C14\[\[^C07(?P<page>.+?)^C14]]^C4 move^C10 ^C02^C ^C5\*^C ^C03(?P<user>.+?)^C ^C5\*^C  ^C10(?P<action>.+?) \[\[^C02(?P<frompage>.+?)^C10]] to \[\[(?P<topage>.+?)]]((?P<summary>.*))?^C'.replace('^C', '\003'))
+            r'^C14\[\[^C07(?P<page>.+?)^C14]]^C4 move^C10 ^C02^C ^C5\*^C ^C03(?P<user>.+?)^C ^C5\*^C  ^C10(?P<action>.+?) \[\[^C02(?P<frompage>.+?)^C10]] to \[\[(?P<topage>.+?)]]((?P<summary>.*))?^C'.replace('^C', '\003'))
         self.re_move_redir = re.compile(
-            ur'^C14\[\[^C07(?P<page>.+?)^C14]]^C4 move_redir^C10 ^C02^C ^C5\*^C ^C03(?P<user>.+?)^C ^C5\*^C  ^C10(?P<action>.+?) \[\[^C02(?P<frompage>.+?)^C10]] to \[\[(?P<topage>.+?)]] over redirect: ((?P<summary>.*))?^C'.replace('^C', '\003'))
+            r'^C14\[\[^C07(?P<page>.+?)^C14]]^C4 move_redir^C10 ^C02^C ^C5\*^C ^C03(?P<user>.+?)^C ^C5\*^C  ^C10(?P<action>.+?) \[\[^C02(?P<frompage>.+?)^C10]] to \[\[(?P<topage>.+?)]] over redirect: ((?P<summary>.*))?^C'.replace('^C', '\003'))
         self.re_delete_redir = re.compile(
-            ur'^C14\[\[^C07(?P<page>.+?)^C14]]^C4 delete_redir^C10 ^C02^C ^C5\*^C ^C03(?P<user>.+?)^C ^C5\*^C  ^C10(?P<action>.+?) \[\[^C02(?P<frompage>.+?)^C10\]\](?P<reason>.*?):(?P<comment>.*?„\[\[(?P<topage>.*?\]\])”)^C'.replace('^C', '\003'))
+            r'^C14\[\[^C07(?P<page>.+?)^C14]]^C4 delete_redir^C10 ^C02^C ^C5\*^C ^C03(?P<user>.+?)^C ^C5\*^C  ^C10(?P<action>.+?) \[\[^C02(?P<frompage>.+?)^C10\]\](?P<reason>.*?):(?P<comment>.*?„\[\[(?P<topage>.*?\]\])”)^C'.replace('^C', '\003'))
 
     def on_nicknameinuse(self, c, e):
         c.nick(c.get_nickname() + "_")
@@ -75,10 +78,10 @@ class ArtNoDisp(SingleServerIRCBot):
         source = e.source().split ( '!' ) [ 0 ]
         text = e.arguments() [ 0 ]
         #print text
-        #pywikibot.output(u'CONNECTION:%s' % unicode(c[ 0 ], 'utf-8'))
-        #pywikibot.output(u'SOURCE:%s' % source)
-        #if u'move' in text:
-        #    pywikibot.output(u'TEXT move:%s' % text)
+        #pywikibot.output('CONNECTION:%s' % unicode(c[ 0 ], 'utf-8'))
+        #pywikibot.output('SOURCE:%s' % source)
+        #if 'move' in text:
+        #    pywikibot.output('TEXT move:%s' % text)
     
         edit = False
         move = False
@@ -92,31 +95,31 @@ class ArtNoDisp(SingleServerIRCBot):
 
         if match:
             edit = True
-            #pywikibot.output(u'EDIT')
+            #pywikibot.output('EDIT')
         elif matchmove:
             move = True
-            #pywikibot.output(u'MOVE')
+            #pywikibot.output('MOVE')
         elif matchmoveredir:
             move_redir = True
             matchmove = matchmoveredir
-            #pywikibot.output(u'MOVE_REDIR')
+            #pywikibot.output('MOVE_REDIR')
         elif matchdeleteredir:
             delete_redir = True
             matchmove = matchdeleteredir
-            #pywikibot.output(u'DELETE_REDIR')
+            #pywikibot.output('DELETE_REDIR')
 
         if move or move_redir or delete_redir:
             mvpagefrom = unicode(matchmove.group('frompage'), 'utf-8') 
             mvpageto = unicode(matchmove.group('topage'), 'utf-8')        
-            #mvactionu = unicode(matchmove.group('actionu'), 'utf-8')        
+            #mvactionu = unicode(matchmove.group('action'), 'utf-8')        
             mvaction = unicode(matchmove.group('action'), 'utf-8')
             if matchmove.group('summary'):
                 mvsummary = unicode(matchmove.group('summary'), 'utf-8')
             else:
-                mvsummary = u''
+                mvsummary = ''
             mvuser = unicode(matchmove.group('user'), 'utf-8')
             currtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            pywikibot.output (u'MOVE->F:%s:T:%s:AT:%s:S:%s:SU:%s:T:%s' % (mvpagefrom,mvpageto,mvaction,mvuser,mvsummary,currtime))
+            pywikibot.output ('MOVE->F:%s:T:%s:AT:%s:S:%s:SU:%s:T:%s' % (mvpagefrom,mvpageto,mvaction,mvuser,mvsummary,currtime))
             frompage = pywikibot.Page(self.site, mvpagefrom)
             topage = pywikibot.Page(self.site, mvpageto)
             if topage.namespace() == 0 and not frompage.namespace == 0:
@@ -125,18 +128,18 @@ class ArtNoDisp(SingleServerIRCBot):
                 #register edit
                 text = self.site.getUrl(self.apiURL)
                 #print text.encode('UTF-8')
-                artsR = re.compile(ur'articles="(?P<arts>.*?)"')
+                artsR = re.compile(r'articles="(?P<arts>.*?)"')
                 match = artsR.search(text)
                 arts = match.group('arts')
-                #pywikibot.output(u'Liczba artykułów:%s' % arts)
+                #pywikibot.output('Liczba artykułów:%s' % arts)
                 logfile = open(self.logname,"a")
                 if topage.isRedirectPage():
                     try:
-                        logline = arts + ';' + currtime + ';RM;' + mvpageto +';' + topage.getRedirectTarget().title() + u'\n'
+                        logline = arts + ';' + currtime + ';RM;' + mvpageto +';' + topage.getRedirectTarget().title() + '\n'
                     except pywikibot.exceptions.CircularRedirect:
-                        logline = arts + ';' + currtime + ';R;' + mpage +';' + mpage + u'\n'
+                        logline = arts + ';' + currtime + ';R;' + mpage +';' + mpage + '\n'
                 else:
-                    logline = arts + ';' + currtime + ';AM;' + mvpageto + u';\n'
+                    logline = arts + ';' + currtime + ';AM;' + mvpageto + ';\n'
                 pywikibot.output(logline.encode('UTF-8'))
                 logfile.write(logline.encode('UTF-8'))
                 logfile.close()
@@ -149,10 +152,10 @@ class ArtNoDisp(SingleServerIRCBot):
             mpage = unicode(match.group('page'), 'utf-8')        
             msummary = unicode(match.group('summary'), 'utf-8')
             currtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            #print (u'P:%s:F:%s:U:%s:B:%s:S:%s:U:%s:T:%s' % (mpage,mflags,muser,mbytes,msummary,murl,currtime)).encode('UTF-8')
+            #print ('P:%s:F:%s:U:%s:B:%s:S:%s:U:%s:T:%s' % (mpage,mflags,muser,mbytes,msummary,murl,currtime)).encode('UTF-8')
             newArt = 'N' in mflags
             page = pywikibot.Page(self.site, mpage)
-            #print (u'P:%s:F:%s:U:%s:B:%s:S:%s:U:%s:T:%s:NS:%i' % (mpage,mflags,muser,mbytes,msummary,murl,currtime,page.namespace())).encode('UTF-8')
+            #print ('P:%s:F:%s:U:%s:B:%s:S:%s:U:%s:T:%s:NS:%i' % (mpage,mflags,muser,mbytes,msummary,murl,currtime,page.namespace())).encode('UTF-8')
 
             if newArt and (page.namespace() == 0):
                 # try threading
@@ -160,7 +163,7 @@ class ArtNoDisp(SingleServerIRCBot):
                     #NAthread = newArticleThread((page,))
                     pass
                 else:
-                    pywikibot.output(u'skipping')
+                    pywikibot.output('skipping')
 
                 #print 'new article'
                 #currtime = strftime("%Y-%m-%d %H:%M:%S", datetime.datetime.now().time())
@@ -168,18 +171,18 @@ class ArtNoDisp(SingleServerIRCBot):
                 #register edit
                 text = self.site.getUrl(self.apiURL)
                 #print text.encode('UTF-8')
-                artsR = re.compile(ur'articles="(?P<arts>.*?)"')
+                artsR = re.compile(r'articles="(?P<arts>.*?)"')
                 match = artsR.search(text)
                 arts = match.group('arts')
-                #pywikibot.output(u'Liczba artykułów:%s' % arts)
+                #pywikibot.output('Liczba artykułów:%s' % arts)
                 logfile = open(self.logname,"a")
                 if page.isRedirectPage():
                     try:
-                        logline = arts + ';' + currtime + ';R;' + mpage +';' + page.getRedirectTarget().title() + u'\n'
+                        logline = arts + ';' + currtime + ';R;' + mpage +';' + page.getRedirectTarget().title() + '\n'
                     except pywikibot.exceptions.CircularRedirect:
-                        logline = arts + ';' + currtime + ';R;' + mpage +';' + mpage + u'\n'
+                        logline = arts + ';' + currtime + ';R;' + mpage +';' + mpage + '\n'
                 else:
-                    logline = arts + ';' + currtime + ';A;' + mpage + u';\n'
+                    logline = arts + ';' + currtime + ';A;' + mpage + ';\n'
                 pywikibot.output(logline.encode('UTF-8'))
                 logfile.write(logline.encode('UTF-8'))
                 logfile.close()
@@ -187,7 +190,7 @@ class ArtNoDisp(SingleServerIRCBot):
             #elif page.namespace() == 2:
             #    UPthread = userPageThread((page,))
             else:
-                pywikibot.output(u'[%s] Skipping:%s' % (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),page.title()))
+                pywikibot.output('[%s] Skipping:%s' % (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),page.title()))
     def on_dccmsg(self, c, e):
         pass
 
@@ -203,14 +206,14 @@ class ArtNoDisp(SingleServerIRCBot):
 
 def savepid(suffix):
         """ get script name and pid, save it in scriptname.pid"""
-        scrR = ur'.*\/(.*?)\.py'
-        script = re.sub(scrR,ur'\1',sys.argv[0])
+        scrR = r'.*\/(.*?)\.py'
+        script = re.sub(scrR,r'\1',sys.argv[0])
         pid = os.getpid()
-        print script
-        print pid
-        pidname = u'masti/pid/' + script + suffix + u'.pid'
+        print(script)
+        print(pid)
+        pidname = 'masti/pid/' + script + suffix + '.pid'
         pidfile = open(pidname,"w")
-        pidfile.write(str(pid)+u'\n')
+        pidfile.write(str(pid)+'\n')
         pidfile.close()
         return
 
@@ -222,7 +225,7 @@ def main():
             lang = arg[6:]
         if arg.startswith('-name:'):
             botname = arg[6:]
-    savepid(u'-'+lang)
+    savepid('-'+lang)
     site = pywikibot.Site(lang,fam='wikipedia')
     #site.forceLogin()
     chan = '#' + site.language() + '.' + site.family.name
